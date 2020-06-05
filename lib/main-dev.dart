6ce573194032098bd471bf5682c942c4060b04f9
madz <markdiesta@gmail.com>
2020-06-05 08:18:27 +0800

    Update main-dev.dart

--- lib/main-dev.dart
+++ lib/main-dev.dart
@@ -1,14 +1,5 @@
 import 'package:blochub/common_main.dart';
-import 'package:firebase_crashlytics/firebase_crashlytics.dart';
-import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:injectable/injectable.dart';
-
-import 'app.dart';
-import 'core/di/injector.dart';
-import 'simple_bloc_delegate.dart';
 
 void main() async {
   String env = Environment.dev;
